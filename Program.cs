15d73b18e1e33a2cec0225727d2d0aa93e66f2ea
rahu619 <rahulrajan@techie.com>
2021-08-24 17:15:29 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+//using Microsoft.Windows.
+
+
+namespace windows.search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //opening up a new explorer window
+            Process.Start("explorer.exe");
+            IntPtr MyHwnd = WinAPI.FindWindowA(null, "File Explorer");
+
+            //using that handle to set the window name
+            WinAPI.SetWindowTextA(MyHwnd, "Test");
+
+            Console.ReadKey();
+        }
+
+        static void PerformSearch()
+        {
+
+
+        }
+    }
+}
---
+++ WinAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace windows.search
+{
+    public class WinAPI
+    {
+        [DllImport("User32.dll")]
+        public static extern bool SetWindowTextA(IntPtr hWnd, string lpString);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr FindWindowA(string lpClassName, string lpWindowName);
+
+        //[DllImport("User32.dll")]
+        //public static extern IntPtr FindTextA(LPFINDREPLACEA unnamedParam1);
+
+    }
+}
