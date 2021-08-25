cfb29c988c32d332dd88fda63a21023af9bec0d2
rahu619 <rahulrajan@techie.com>
2021-08-25 10:48:51 +0200

    Saving changess

--- Program.cs
+++ Program.cs
@@ -11,21 +11,33 @@ namespace windows.search
     {
         static void Main(string[] args)
         {
-
             //opening up a new explorer window
-            Process.Start("explorer.exe");
+            //Process.Start("explorer.exe");
+
             IntPtr MyHwnd = WinAPI.FindWindowA(null, "File Explorer");
 
-            //using that handle to set the window name
-            WinAPI.SetWindowTextA(MyHwnd, "Test");
+            ////using that handle to set the window name
+            WinAPI.SetWindowTextA(MyHwnd, "Test Title");
+
+            //Process.Start(srch);
+
+
+            //WinAPI.ShellExecute(MyHwnd, "open", "explorer.exe", @"C:\Repos\aiconomix\windows.search", null, WinAPI.ShowCommands.SW_NORMAL);
+
+            //WinAPI.ShellExecute(IntPtr.Zero, "explore", @"C:\Repos\aiconomix\windows.search", null, null, WinAPI.ShowCommands.SW_NORMAL);
+
+            //WinAPI.ShellExecute(IntPtr.Zero, "explore", @"C:\Repos\aiconomix\windows.search", null, null, WinAPI.ShowCommands.SW_NORMAL);
+
+            WinAPI.ShellExecute(MyHwnd, "find", @"C:\Repos\aiconomix\windows.search", null, null, WinAPI.ShowCommands.SW_HIDE);
+            //ShellExecute(handle, "find", < fully_qualified_path_to_folder >, NULL, NULL, 0);
+            //var shellApplication = Type.GetTypeFromProgID("Shell.Application");
+            //dynamic shell = Activator.CreateInstance(shellApplication);
+            //shell.Open(@"C:\Repos\");
 
             Console.ReadKey();
         }
 
-        static void PerformSearch()
-        {
+ 
 
-
-        }
     }
 }
--- WinAPI.cs
+++ WinAPI.cs
@@ -11,8 +11,33 @@ namespace windows.search
         [DllImport("User32.dll")]
         public static extern IntPtr FindWindowA(string lpClassName, string lpWindowName);
 
-        //[DllImport("User32.dll")]
-        //public static extern IntPtr FindTextA(LPFINDREPLACEA unnamedParam1);
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, uint Msg, uint wParam, int lParam);
+
+
+        [DllImport("shell32.dll")]
+        public static extern IntPtr ShellExecute(IntPtr hwnd, string lpOperation, string lpFile, string lpParameters, string lpDirectory, ShowCommands nShowCmd);
+
+        public enum ShowCommands : int
+        {
+            SW_HIDE = 0,
+            SW_SHOWNORMAL = 1,
+            SW_NORMAL = 1,
+            SW_SHOWMINIMIZED = 2,
+            SW_SHOWMAXIMIZED = 3,
+            SW_MAXIMIZE = 3,
+            SW_SHOWNOACTIVATE = 4,
+            SW_SHOW = 5,
+            SW_MINIMIZE = 6,
+            SW_SHOWMINNOACTIVE = 7,
+            SW_SHOWNA = 8,
+            SW_RESTORE = 9,
+            SW_SHOWDEFAULT = 10,
+            SW_FORCEMINIMIZE = 11,
+            SW_MAX = 11
+        }
+
 
     }
+
 }
